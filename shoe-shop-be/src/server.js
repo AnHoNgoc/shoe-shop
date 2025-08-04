2b9358739ff8de654244aec9dc41c916ad28e6bc
HoNgocAn <anhongoc1306@gmail.com>
2025-08-04 22:55:27 +0700

    up

--- shoe-shop-be/src/server.js
+++ shoe-shop-be/src/server.js
@@ -1,5 +1,4 @@
-import dotenv from 'dotenv';
-dotenv.config();
+require('dotenv').config();
 import express from "express";
 import connection from "./config/connectionDB.js";
 import initAPIbRoutes from "./routes/api.js";
